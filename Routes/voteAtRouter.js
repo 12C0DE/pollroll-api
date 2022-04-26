ea7928f6f3af882231dbad5d54712048d6b13693
Ruben Hernandez <rubenhnt@gmail.com>
2022-04-25 21:38:40 -0500

    create pollId object

--- Routes/voteAtRouter.js
+++ Routes/voteAtRouter.js
@@ -3,7 +3,8 @@ const res = require("express/lib/response");
 const router = express.Router();
 
 router.get("/pollId", (req, res) => {
-  res.json(req.cookies.pollId);
+  const pollId = { pollId: req.cookies.pollId };
+  res.json(pollId);
 });
 
 router.get("/add/:pollId", (req, res) => {
