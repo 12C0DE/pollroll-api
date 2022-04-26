7d3ec43f629c5d29179c3b09b806b4d93d840137
Ruben Hernandez <rubenhnt@gmail.com>
2022-04-25 23:46:15 -0500

    add comments

--- Routes/voteAtRouter.js
+++ Routes/voteAtRouter.js
@@ -2,16 +2,19 @@ const express = require("express");
 const res = require("express/lib/response");
 const router = express.Router();
 
+//retrieve pollid
 router.get("/pollId", (req, res) => {
   res.json(res.cookies("pollId"));
 });
 
+//create pollId cookie
 router.get("/add/:pollId", (req, res) => {
   res
     .cookie("pollId", req.params.pollId, { expire: 600000 + Date.now() })
     .redirect("https://www.pollroll.net");
 });
 
+//clear pollId cookie
 router.get("/clear", (req, res) => {
   res.clearCookie("pollId").sendStatus(204);
 });
