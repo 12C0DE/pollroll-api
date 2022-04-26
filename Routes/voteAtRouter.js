f69529992fc9a45b7ba591c89a0739dc3781c1a2
Ruben Hernandez <rubenhnt@gmail.com>
2022-04-25 23:36:49 -0500

    res.json

--- Routes/voteAtRouter.js
+++ Routes/voteAtRouter.js
@@ -3,7 +3,7 @@ const res = require("express/lib/response");
 const router = express.Router();
 
 router.get("/pollId", (req, res) => {
-  res.send(res.cookies("pollId"));
+  res.json(res.cookies("pollId"));
 });
 
 router.get("/add/:pollId", (req, res) => {
